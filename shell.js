018437a1f9144581160f462bbc3be9c8838c042b
郭月新 <wb-gyx641107@koubei.com>
2020-11-17 18:26:21 +0800

    feat：修改时间

--- shell.js
+++ shell.js
@@ -42,7 +42,7 @@ setInterval(() => {
       shell.exec('node jd_pet.js');
     }
   
-    if (hour === 12) {
+    if (hour === 1) {
       // 京小超兑换奖品
       if (minute === 0) {
         shell.exec('node jd_blueCoin.js');
